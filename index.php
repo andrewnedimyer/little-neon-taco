afa34bf450c8180fe3309f6ef80268be3a78ffb8
Andrew Nedimyer <anedimye@anedimye010740.starbucks.net>
2018-04-27 12:42:29 -0700

    adding files to github

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>🏄</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Facebook / Google meta info -->
+	<link rel="canonical" href="http://www.website.com/" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Website Name Here" />
+	<meta property="og:description" content="Description of website here" />
+	<meta property="og:url" content="http://www.website.com/" />
+	<meta property="og:site_name" content="Website name here" />
+	<meta property="og:image" content="/img/facebook-share.png" />
+
+	<!-- Twitter Meta info -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="http://www.website.com/" />
+	<meta name="twitter:title" content="Website name here" />
+	<meta name="twitter:description" content="Description of website here" />
+	<meta name="twitter:image" content="/img/facebook-share.png" />
+
+	<!-- Favicon -->
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+	<meta name="application-name" content="&nbsp;"/>
+	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<meta name="msapplication-TileImage" content="/img/mstile-144x144.png" />
+	
+	<!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" media="all">
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,600,600i" rel="stylesheet">
+
+	
+</head>
+<body>
+
+<div class="content">
+	<div class="grid">
+		<div class="row padded60">
+			<div class="c12">
+				<div class="logo"><h1>Little Neon Taco–Seattle, WA</h1></div>
+			</div>
+			<div class="top-fixed">
+				<div class="small-logo"></div>
+				<div class="address">1011 Boren Ave, Seattle, WA 98104</div>
+			</div>
+
+			<div class="c6 space-top150">
+				<div class="hours">
+					<p>A modern Mexican taqueria<br>
+						Open 9am–2am, Monday thru Saturday<br>
+						Closed on Sundays
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="g1024">
+		<div class="row padded60">
+			<div class="c6">
+				<h2>Tacos—</h2>
+				<ul class="menu space-top">
+					<li><div class="menu-title">Carnitas — </div><div class="menu-description">pulled pork, green salsa, onion & cilantro</div></li>
+					<li><div class="menu-title">Cueritos — </div><div class="menu-description">soft pork rinds, citrus, serrano, cabbage</div></li>
+					<li><div class="menu-title">Mole — </div><div class="menu-description">chicken mole, crema, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Picadillo — </div><div class="menu-description">ground beef, cabbage, tapatio </div></li>
+					<li><div class="menu-title">Chorizo — </div><div class="menu-description">pork chorizo, potatoes, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Mole — </div><div class="menu-description">chicken mole, crema, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Picadillo — </div><div class="menu-description">ground beef, cabbage, tapatio </div></li>
+					<li><div class="menu-title">Chorizo — </div><div class="menu-description">pork chorizo, potatoes, cotija, pickled onion</div></li>
+				</ul>
+			</div>
+
+			<div class="c6">
+				<h2>Not Tacos—</h2>
+				<ul class="menu space-top">
+					<li><div class="menu-title">Carnitas — </div><div class="menu-description">pulled pork, green salsa, onion & cilantro</div></li>
+					<li><div class="menu-title">Cueritos — </div><div class="menu-description">soft pork rinds, citrus, serrano, cabbage</div></li>
+					<li><div class="menu-title">Mole — </div><div class="menu-description">chicken mole, crema, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Picadillo — </div><div class="menu-description">ground beef, cabbage, tapatio </div></li>
+					<li><div class="menu-title">Chorizo — </div><div class="menu-description">pork chorizo, potatoes, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Mole — </div><div class="menu-description">chicken mole, crema, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Picadillo — </div><div class="menu-description">ground beef, cabbage, tapatio </div></li>
+					<li><div class="menu-title">Chorizo — </div><div class="menu-description">pork chorizo, potatoes, cotija, pickled onion</div></li>
+				</ul>
+			</div>
+
+			<div class="c6 space-top60">
+				<h2>Chips & Dip—</h2>
+				<ul class="menu space-top">
+					<li><div class="menu-title">Carnitas — </div><div class="menu-description">pulled pork, green salsa, onion & cilantro</div></li>
+					<li><div class="menu-title">Cueritos — </div><div class="menu-description">soft pork rinds, citrus, serrano, cabbage</div></li>
+					<li><div class="menu-title">Mole — </div><div class="menu-description">chicken mole, crema, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Picadillo — </div><div class="menu-description">ground beef, cabbage, tapatio </div></li>
+					<li><div class="menu-title">Chorizo — </div><div class="menu-description">pork chorizo, potatoes, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Mole — </div><div class="menu-description">chicken mole, crema, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Picadillo — </div><div class="menu-description">ground beef, cabbage, tapatio </div></li>
+					<li><div class="menu-title">Chorizo — </div><div class="menu-description">pork chorizo, potatoes, cotija, pickled onion</div></li>
+				</ul>
+			</div>
+
+			<div class="c6 space-top60">
+				<h2>Drinks—</h2>
+				<ul class="menu space-top">
+					<li><div class="menu-title">Carnitas — </div><div class="menu-description">pulled pork, green salsa, onion & cilantro</div></li>
+					<li><div class="menu-title">Cueritos — </div><div class="menu-description">soft pork rinds, citrus, serrano, cabbage</div></li>
+					<li><div class="menu-title">Mole — </div><div class="menu-description">chicken mole, crema, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Picadillo — </div><div class="menu-description">ground beef, cabbage, tapatio </div></li>
+					<li><div class="menu-title">Chorizo — </div><div class="menu-description">pork chorizo, potatoes, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Mole — </div><div class="menu-description">chicken mole, crema, cotija, pickled onion</div></li>
+					<li><div class="menu-title">Picadillo — </div><div class="menu-description">ground beef, cabbage, tapatio </div></li>
+					<li><div class="menu-title">Chorizo — </div><div class="menu-description">pork chorizo, potatoes, cotija, pickled onion</div></li>
+				</ul>
+			</div>
+			
+		</div>
+	</div><!-- /grid -->
+	<div class="gmax">
+		<div class="row">
+			<div class="pink-wave"></div>
+		</div>
+	</div>
+</div><!-- /content -->
+
+<!-- JS -->
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
+<script type='text/javascript' src='/js/main.js'></script>
+
+<!-- Analytics -->
+
+</body>
+</html>
